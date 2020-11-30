793b98e8b49c710714f24b719ed4191ec1d790ab
Luana Albertoni <65794490+LuanaAlbertoni@users.noreply.github.com>
2020-11-30 14:53:38 -0300

    Modulo 1 2 e 3

---
+++ modulo1/condiçãoTernária.js
@@ -0,0 +1,15 @@
+// Condição ternária
+
+var sexo = 'M'
+
+if (sexo === 'M'){
+    return : 'Masculino'
+} else {
+    return : 'Feminino'
+}
+
+
+// Também pode ser:
+
+  return (sexo === 'M') ? 'Masculino' : 'Feminino'
+
